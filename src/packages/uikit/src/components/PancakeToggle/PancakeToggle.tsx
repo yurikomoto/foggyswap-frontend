902b406873df2f5d263f5bee0c3a6280ea7c46d6
Altbee <liaojikundever@gmail.com>
2021-09-15 19:33:13 -0500

    update

--- src/packages/uikit/src/components/PancakeToggle/PancakeToggle.tsx
+++ src/packages/uikit/src/components/PancakeToggle/PancakeToggle.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import { PancakeStack, PancakeInput, PancakeLabel } from "./StyledPancakeToggle";
-import { PancakeToggleProps, scales } from "./types";
+import React from 'react'
+import { PancakeStack, PancakeInput, PancakeLabel } from './StyledPancakeToggle'
+import { PancakeToggleProps, scales } from './types'
 
 const PancakeToggle: React.FC<PancakeToggleProps> = ({ checked, scale = scales.LG, ...props }) => (
   <PancakeStack scale={scale}>
-    <PancakeInput id={props.id || "pancake-toggle"} scale={scale} type="checkbox" checked={checked} {...props} />
-    <PancakeLabel scale={scale} checked={checked} htmlFor={props.id || "pancake-toggle"}>
+    <PancakeInput id={props.id || 'pancake-toggle'} scale={scale} type="checkbox" checked={checked} {...props} />
+    <PancakeLabel scale={scale} checked={checked} htmlFor={props.id || 'pancake-toggle'}>
       <div className="pancakes">
-        <div className="pancake" />
-        <div className="pancake" />
-        <div className="pancake" />
-        <div className="butter" />
+        <img alt="tst" className="pancake" src="/images/toggle/wagyu.png" />
       </div>
     </PancakeLabel>
   </PancakeStack>
-);
+)
 
-export default PancakeToggle;
+export default PancakeToggle
