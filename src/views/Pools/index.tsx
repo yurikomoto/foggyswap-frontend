875f82f4745c4e21aed2b7daed0e9b3a1f340ace
Altbee <liaojikundever@gmail.com>
2021-09-20 10:56:37 -0500

    update poolPage hero labels

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -257,13 +257,13 @@ const Pools: React.FC = () => {
         <Flex justifyContent="space-between" flexDirection={['column', null, null, 'row']}>
           <Flex flex="1" flexDirection="column" mr={['8px', 0]}>
             <Heading as="h1" scale="xxl" color="secondary" mb="24px">
-              {t('Syrup Pools')}
+              {t('Sizzle Pools')}
             </Heading>
             <Heading scale="md" color="text">
-              {t('Just stake some tokens to earn.')}
+              {t('Stake tokens to get')}
             </Heading>
             <Heading scale="md" color="text">
-              {t('High APR, low risk.')}
+              {t('mouthwatering APR, low risk.')}
             </Heading>
           </Flex>
           <Flex flex="1" height="fit-content" justifyContent="center" alignItems="center" mt={['24px', null, '0']}>
