3f75933fcef15d3d9380b72bc0b0766145e64747
Altbee <liaojikundever@gmail.com>
2021-09-21 20:37:59 -0500

    revert info url

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -13,5 +13,6 @@ export const BITQUERY_API = 'https://graphql.bitquery.io'
  */
 export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
 
-export const INFO_CLIENT = 'http://164.90.190.145:8000/subgraphs/name/wagyu'
+export const INFO_CLIENT = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
+// export const INFO_CLIENT = 'http://164.90.190.145:8000/subgraphs/name/wagyu'
 export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
